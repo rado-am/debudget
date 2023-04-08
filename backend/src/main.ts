f5683b9ccfdc664cca8c54ebecc1ddf28dc214d3
radoam1 <radek@petsy.pl>
2023-04-08 08:06:27 +0200

    Add fastify adapter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const configService = app.get(ConfigService);
 
   const logger = new Logger(bootstrap.name);
