8e4a70331852ab173899596c34ca224cfa01bf94
radoam1 <radek@petsy.pl>
2023-05-21 13:57:41 +0200

    Clean up

--- frontend/src/routes/components/PageLoader/PageLoader.tsx
+++ frontend/src/routes/components/PageLoader/PageLoader.tsx
@@ -1,28 +1,3 @@
 import type { FC } from "react";
 
-import PawIcon from "assets/icons/paw-icon.svg";
-
-import styles from "./PageLoader.module.scss";
-
-export const PageLoader: FC = () => (
-  <div className={styles["page-loader"]}>
-    <div className={styles.paw}>
-      <img src={PawIcon} alt="Paw Icon" className={styles.icon} />
-    </div>
-    <div className={styles.paw}>
-      <img src={PawIcon} alt="Paw Icon" className={styles.icon} />
-    </div>
-    <div className={styles.paw}>
-      <img src={PawIcon} alt="Paw Icon" className={styles.icon} />
-    </div>
-    <div className={styles.paw}>
-      <img src={PawIcon} alt="Paw Icon" className={styles.icon} />
-    </div>
-    <div className={styles.paw}>
-      <img src={PawIcon} alt="Paw Icon" className={styles.icon} />
-    </div>
-    <div className={styles.paw}>
-      <img src={PawIcon} alt="Paw Icon" className={styles.icon} />
-    </div>
-  </div>
-);
+export const PageLoader: FC = () => <div>Loading...</div>;
